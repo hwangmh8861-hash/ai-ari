f5f8747dd8d7d7200684a287ecbf7a0aaef6f8fa
hwangmh8861-hash <hwangmh8861@gmail.com>
2026-04-09 23:50:02 +0900

    Update api.js

--- api.js
+++ api.js
@@ -1,131 +1,34 @@
-/**
- * AI아리 — GAS API 서버
- * GitHub Pages에서 fetch()로 호출하는 용도
- */
+var GAS_URL = 'https://script.google.com/macros/s/AKfycbyO9x9i5_YrLkzaDD3nO7Otsl2-MkITZQbjHXeVc9NDWyYti7hPQU2PIrF3-qgrtiPZEg/exec';
 
-function doGet(e) {
-  return handleRequest(e);
-}
-
-function doPost(e) {
-  return handleRequest(e);
-}
-
-function handleRequest(e) {
-  var params = e.parameter || {};
-  var action = params.action || '';
-  var result = {};
-  
-  try {
-    switch(action) {
-      case 'getCodes':
-        result = getSchoolCodes();
-        break;
-      case 'check':
-        result = checkStudent(params.key);
-        break;
-      case 'register':
-        result = registerStudent(params.key, params.schoolCode, params.grade, params.cls, params.num, params.nick, params.pw, params.data);
-        break;
-      case 'login':
-        result = loginStudent(params.key, params.pw);
-        break;
-      case 'save':
-        result = saveData(params.key, params.data);
-        break;
-      case 'load':
-        result = loadData(params.key);
-        break;
-      default:
-        result = { success: false, error: 'Unknown action' };
+var API = {
+  _call: function(action, params, onSuccess, onFailure) {
+    var url = GAS_URL + '?action=' + action;
+    if (params) {
+      Object.keys(params).forEach(function(k) {
+        url += '&' + k + '=' + encodeURIComponent(params[k]);
+      });
     }
-  } catch(err) {
-    result = { success: false, error: err.toString() };
+    fetch(url)
+      .then(function(res) { return res.json(); })
+      .then(function(data) { if (onSuccess) onSuccess(data); })
+      .catch(function(err) { if (onFailure) onFailure(err); });
+  },
+  getSchoolCodes: function(onSuccess, onFailure) {
+    this._call('getCodes', null, onSuccess, onFailure);
+  },
+  checkStudent: function(key, onSuccess, onFailure) {
+    this._call('check', { key: key }, onSuccess, onFailure);
+  },
+  registerStudent: function(key, schoolCode, grade, cls, num, nick, pw, dataJson, onSuccess, onFailure) {
+    this._call('register', { key: key, schoolCode: schoolCode, grade: grade, cls: cls, num: num, nick: nick, pw: pw, data: dataJson }, onSuccess, onFailure);
+  },
+  loginStudent: function(key, pw, onSuccess, onFailure) {
+    this._call('login', { key: key, pw: pw }, onSuccess, onFailure);
+  },
+  saveData: function(key, dataJson, onSuccess, onFailure) {
+    this._call('save', { key: key, data: dataJson }, onSuccess, onFailure);
+  },
+  loadData: function(key, onSuccess, onFailure) {
+    this._call('load', { key: key }, onSuccess, onFailure);
   }
-  
-  return ContentService.createTextOutput(JSON.stringify(result))
-    .setMimeType(ContentService.MimeType.JSON);
-}
-
-function getSheet(name) {
-  var ss = SpreadsheetApp.getActiveSpreadsheet();
-  var sheet = ss.getSheetByName(name);
-  if (!sheet) {
-    sheet = ss.insertSheet(name);
-    if (name === '학교코드') {
-      sheet.appendRow(['코드', '학교명', '지역', '생성일']);
-      sheet.appendRow(['DEMO2026', '데모학교', '서울', new Date()]);
-      sheet.appendRow(['TEST1234', '테스트학교', '테스트', new Date()]);
-    } else if (name === '학생') {
-      sheet.appendRow(['키', '학교코드', '학년', '반', '번호', '닉네임', '비밀번호', '데이터JSON', '생성일', '수정일']);
-    }
-  }
-  return sheet;
-}
-
-function getSchoolCodes() {
-  var sheet = getSheet('학교코드');
-  var data = sheet.getDataRange().getValues();
-  var codes = {};
-  for (var i = 1; i < data.length; i++) {
-    if (data[i][0]) codes[data[i][0]] = { name: data[i][1], region: data[i][2] };
-  }
-  return codes;
-}
-
-function checkStudent(key) {
-  var sheet = getSheet('학생');
-  var data = sheet.getDataRange().getValues();
-  for (var i = 1; i < data.length; i++) {
-    if (data[i][0] === key) return { exists: true };
-  }
-  return { exists: false };
-}
-
-function registerStudent(key, schoolCode, grade, cls, num, nick, pw, dataJson) {
-  var lock = LockService.getScriptLock();
-  lock.waitLock(10000);
-  try {
-    var check = checkStudent(key);
-    if (check.exists) return { success: false, error: '이미 등록된 학번이야!' };
-    var sheet = getSheet('학생');
-    sheet.appendRow([key, schoolCode, grade, cls, num, nick, pw, dataJson, new Date(), new Date()]);
-    return { success: true };
-  } finally {
-    lock.releaseLock();
-  }
-}
-
-function loginStudent(key, pw) {
-  var sheet = getSheet('학생');
-  var data = sheet.getDataRange().getValues();
-  for (var i = 1; i < data.length; i++) {
-    if (data[i][0] === key) {
-      if (data[i][6] !== pw) return { success: false, error: '비밀번호가 틀렸어!' };
-      return { success: true, data: data[i][7] };
-    }
-  }
-  return { success: false, error: '등록되지 않은 계정이야!' };
-}
-
-function saveData(key, dataJson) {
-  var sheet = getSheet('학생');
-  var data = sheet.getDataRange().getValues();
-  for (var i = 1; i < data.length; i++) {
-    if (data[i][0] === key) {
-      sheet.getRange(i + 1, 8).setValue(dataJson);
-      sheet.getRange(i + 1, 10).setValue(new Date());
-      return { success: true };
-    }
-  }
-  return { success: false };
-}
-
-function loadData(key) {
-  var sheet = getSheet('학생');
-  var data = sheet.getDataRange().getValues();
-  for (var i = 1; i < data.length; i++) {
-    if (data[i][0] === key) return { success: true, data: data[i][7] };
-  }
-  return { success: false };
-}
+};
